4f8dd88cb75b29b06ae800c086441662948ee31b
Roman Schiefer <roman.schiefer@gmail.com>
2015-08-07 08:57:10 +0200

    added infinite scroll on overview page

--- public/js/components/overview.js
+++ public/js/components/overview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AjaxLoader from 'public/js/components/ajaxloader';
 import RestApi from 'public/js/components/restapi';
 import Entry from 'public/js/components/entry';
 import FeedList from 'public/js/components/feedlist';
@@ -7,22 +8,59 @@ import profileData from 'public/js/helper/profiledata';
 export default class Overview extends React.Component {
   constructor(props) {
     super(props);
+    this.lastEntryId = '';
     this.state = {
       entries: [],
       feeds: [],
-      profileData: {}
+      profileData: {},
+      showLoader: false
     };
   }
 
+  fetchAdditionalEntries() {
+    this.setState({showLoader: true});
+    RestApi.getDataFromUrl('/entries/more/' + this.lastEntryId,
+      (data) => {
+        let newData = this.state.entries;
+        data.forEach((entry) => {
+          newData.push(entry);
+        });
+        this.lastEntryId = data[data.length-1]._id;
+        this.setState({
+          entries: newData,
+          showLoader: false
+        });
+    });
+  }
+
   componentWillMount() {
+    this.setState({showLoader: true});
+
     RestApi.getDataFromUrl('/feeds',
       (data) => this.setState({feeds: data}));
 
     RestApi.getDataFromUrl('/entries',
-      (data) => this.setState({entries: data }));
+      (data) => {
+        this.setState({
+          entries: data,
+          showLoader: false,
+        })
+        this.lastEntryId = data[data.length-1]._id;
+
+      });
+  }
+
+  componentDidMount() {
+    $(window).on('scroll', () => {
+      if( $(window).scrollTop() >= $(document).height() - $(window).height() && $(window).scrollTop() > 100) {
+          this.fetchAdditionalEntries();
+      }
+    }).scroll();
   }
 
   render() {
+    const loader = this.state.showLoader ? <AjaxLoader /> : null;
+
     return (
       <div className='col-xs-12'>
         <h1>Overview</h1>
@@ -51,6 +89,7 @@ export default class Overview extends React.Component {
             />
           );
         })}
+        {loader}
       </div>
     );
   }
